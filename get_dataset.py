54b26c65215b52516474e818c4fa2ed166e6b90b
Rocky Xu <34105451+TheRockXu@users.noreply.github.com>
2020-04-30 14:10:11 -0500

    fixed download problem

--- get_dataset.py
+++ get_dataset.py
@@ -2,29 +2,24 @@ import os
 import tensorflow as tf
 
 def download_annotation():
-    if not os.path.exists(os.path.abspath('.')):
-        annotation_zip = tf.keras.utils.get_file('captions.zip',
-                                                 cache_subdir=os.path.abspath('.'),
-                                                 origin='http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
-                                                 extract=True)
-        annotation_file = os.path.dirname(annotation_zip) + '/annotations/captions_train2014.json'
-        os.remove(annotation_zip)
-        print(annotation_file)
-    else:
-        annotation_file = os.path.abspath('.')+'/annotations/captions_train2014.json'
-
+    annotation_zip = tf.keras.utils.get_file('captions.zip',
+                                             cache_subdir=os.path.abspath('.'),
+                                             origin='http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+                                             extract=True)
+    annotation_file = os.path.dirname(annotation_zip) + '/annotations/captions_train2014.json'
+    os.remove(annotation_zip)
     return annotation_file
 
+
 def download_images():
-    if not os.path.exists(os.path.abspath('.')):
-        image_zip = tf.keras.utils.get_file('train2014.zip',
-                                            cache_subdir=os.path.abspath('.'),
-                                            origin='http://images.cocodataset.org/zips/train2014.zip',
-                                            extract=True)
-        PATH = os.path.dirname(image_zip)
-        os.remove(image_zip)
-    else:
-        PATH = os.path.abspath('.')
+
+    image_zip = tf.keras.utils.get_file('train2014.zip',
+                                        cache_subdir=os.path.abspath('.'),
+                                        origin='http://images.cocodataset.org/zips/train2014.zip',
+                                        extract=True)
+    PATH = os.path.dirname(image_zip)
+    os.remove(image_zip)
+
 
     return PATH
 
